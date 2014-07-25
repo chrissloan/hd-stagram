10e0020dfb5e7195efb17689ab24dc11b34871f2
Chris Sloan <chris@chrissloan.info>
2014-07-24 23:25:06 -0400

    Karma config file

---
+++ karma.conf.js
@@ -0,0 +1,14 @@
+module.exports = function(config) {
+  config.set({
+    frameworks: ['jasmine'],
+
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'src/**/*.js',
+      'test/*.spec.js'
+    ],
+    reporters: ['progress'],
+    colors: true
+  });
+};
