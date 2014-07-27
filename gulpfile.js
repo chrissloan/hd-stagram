0193ae3dfcc63fd527d0f250bb42977338d94285
Chris Sloan <chris@chrissloan.info>
2014-07-27 15:16:06 -0400

    Adding ng-html2js for templating

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,38 @@
-var gulp   = require('gulp'),
-    coffee = require('gulp-coffee'),
-    concat = require('gulp-concat'),
-    ngmin  = require('gulp-ngmin'),
-    uglify = require('gulp-uglify'),
-    gutil  = require('gulp-util'),
-    karma  = require('gulp-karma');
+var gulp      = require('gulp'),
+    coffee    = require('gulp-coffee'),
+    concat    = require('gulp-concat'),
+    ngmin     = require('gulp-ngmin'),
+    uglify    = require('gulp-uglify'),
+    gutil     = require('gulp-util'),
+    karma     = require('gulp-karma'),
+    ngHtml2Js = require("gulp-ng-html2js");
 
-var file_name     = 'ngStagram.js',
-    min_file_name = 'ngStagram.min.js';
+var file_name     = 'hdStagram.js',
+    min_file_name = 'ndStagram.min.js';
 
 var paths = {
-  coffee  : './src/coffee/*.coffee',
-  js_dest : './src/js/',
-  js      : './src/js/*.js',
-  dist    : './dist/',
-  min     : './dist/ngStagram.min.js',
-  test    : './test/*.spec.js'
+  coffee   : './src/coffee/*.coffee',
+  js_dest  : './src/js/',
+  js       : './src/js/*.js',
+  dist     : './dist/',
+  min      : './dist/ngStagram.min.js',
+  test     : './test/*.spec.js',
+  templates: './src/templates/*.html'
 };
 
 gulp.task('coffee', function() {
   gulp.src(paths.coffee)
     .pipe(coffee({bare: true})).on('error', gutil.log)
     .pipe(gulp.dest(paths.js_dest))
+});
+
+gulp.task('templates', function(){
+  gulp.src(paths.templates)
+    .pipe(ngHtml2Js({
+        moduleName: "hdStagramTemplates",
+        prefix: "/templates/"
+    }))
+    .pipe(gulp.dest(paths.js_dest));
 });
 
 gulp.task('concat', function() {
@@ -63,7 +74,8 @@ gulp.task('build', ['concat', 'minify', 'uglify']);
 
 gulp.task('watch', function(){
   gulp.watch(paths.coffee, ['coffee']);
+  gulp.watch(paths.templates, ['templates']);
   gulp.watch(paths.js, ['build']);
 });
 
-gulp.task('default', ['build', 'watch']);
+gulp.task('default', ['coffee', 'build', 'watch']);
